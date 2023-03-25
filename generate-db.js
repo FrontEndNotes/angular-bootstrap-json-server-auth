ccc01ddcff6f2ab8a746c80ae7fc0a388d761521
Ricardo <ricardo@example.com>
2023-03-25 18:56:39 +0100

    feat(faker): generate fake data on run - users and posts

---
+++ generate-db.js
@@ -0,0 +1,60 @@
+const { faker } = require('@faker-js/faker');
+
+const userCount = 4;
+const postsPerUserCount = 4;
+
+// users
+const users = [...Array(userCount)].map((_, i) => ({
+    id: i + 1,
+    fullName: faker.name.fullName(),
+    birthDate: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }),
+    avatar: faker.image.avatar(),
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    bio: faker.lorem.sentences(1),
+}));
+
+// posts
+const posts = users.reduce((acc, user) => {
+
+    const imageCategories = [
+        "abstract",
+        "animals",
+        "business",
+        "cats",
+        "city",
+        "food",
+        "nightlife",
+        "fashion",
+        "people",
+        "nature",
+        "sports",
+        "technics",
+        "transport",
+    ];
+
+    const userPosts = [...Array(postsPerUserCount)].map((_, i) => {
+        const imageCategory = imageCategories[Math.floor(Math.random() * imageCategories.length)];
+
+        return {
+            id: i + 1 + (user.id - 1) * userCount,
+            userId: user.id,
+            title: faker.lorem.sentence(),
+            body: faker.lorem.paragraph(2),
+            category: imageCategory,
+            publicationDate: faker.date.past(),
+            images: [...Array(Math.floor(Math.random() * 5) + 1)].map(
+                () => faker.image[imageCategory](800, 600, true)
+            )
+        };
+    });
+    return [...acc, ...userPosts];
+}, []);
+
+// db
+const db = { users, posts };
+
+// console.log is required to write its content to a file later on
+// 4 - spaces indent
+console.log(JSON.stringify(db, null, 4));
